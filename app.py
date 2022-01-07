9c43b68725f833f86f70ffa8b0f75aed39d18523
Yuvraj Virdi <81879713+yuvrajvirdi@users.noreply.github.com>
2022-01-06 22:22:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from re import S
 from flask import Flask, render_template, request
-from scraper import get_data
-from graph import make_graph
+from scrapers import get_data, get_financials, get_holders, get_profile, get_analytics
+from graph import make_graph, make_rev_graph, make_pie_graph
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def get_stock():
     if request.method == "POST":
-        stock_name = request.form.get('sname')
-        period = request.form.get('period')
+        stock_name = request.form.get('stock')
+
         data = get_data(stock_name)
-        stock_graph = make_graph(stock_name, period)
-        return render_template('card.html', data=data)
+        financial_data = get_financials(stock_name)
+        holders_data = get_holders(stock_name)
+        profile = get_profile(stock_name)
+        growth = get_analytics(stock_name)
+
+        candle_graph = make_graph(stock_name)
+        rev_graph = make_rev_graph(financial_data)
+        pie_graph = make_pie_graph(holders_data)
+
+        return render_template('card.html', data=data, profile=profile, growth=growth)
+
     else:
         return render_template('index.html')
-    
+
 @app.errorhandler(500)
 def handle_500(e):
     error = 'Please enter valid inputs'
